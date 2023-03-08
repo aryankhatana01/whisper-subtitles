f5359d1a68c485735a46d2a0db12a36011829527
aryankhatana01 <aryankhatana01@gmail.com>
2023-03-08 20:52:46 +0530

    some UI fixes + footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>Made By - Aryan Khatana🙌🏻</div>
+  )
+}
+
+export default Footer
